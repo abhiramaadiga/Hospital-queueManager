a61210b718279383f3e5e626f75256bada5cf042
Abhirama Adiga <adigaabhirama@gmail.com>
2026-05-12 23:35:51 +0530

    Fix frontend as normal folder

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router ,Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Register from './pages/Register';
+import Queue from './pages/Queue';
+import History from  './pages/History';
+import Analytics from './pages/Analytics';
+
+function App() {
+   return (
+    <Router>
+      <Navbar/>
+      <Routes>
+         <Route path="/" element={<Register />} />
+        <Route path="/queue" element={<Queue />} />
+        <Route path="/history" element={<History />} />
+        <Route path="/analytics" element={<Analytics />} />
+      </Routes>
+    </Router>
+   )
+}
